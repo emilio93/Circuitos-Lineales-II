e1803fceb5030eb69e82795ac0285a43ea63e1e4
Emilio Rojas <emilio93@gmail.com>
2016-08-14 00:30:35 -0600

    remove obsolete build system

--- Apuntes/build.bat
+++
@@ -1,8 +0,0 @@
-if not exist "build" mkdir build
-
-cd build
-del *.pdf *.aux *.log *.out *.toc *.blg *.bbl 2>nul
-cd ..
-
-pdflatex main -output-directory=build
-pdflatex main -output-directory=build
