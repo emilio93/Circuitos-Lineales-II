40951aee366e72af285cd398b25557a421983e7c
Emilio Rojas <emilio93@gmail.com>
2016-08-15 08:02:07 -0600

    remove pre from md s

--- js/source.js
+++ js/source.js
@@ -21,14 +21,14 @@ $(document).ready(function() {
   function loadNotes() {
     $('#contenido > div > div').html('<p class="text-center"><i class="fa fa-spinner fa-spin"></i></span>');
     $.ajax('https://raw.githubusercontent.com/emilio93/Circuitos-Lineales-II/gh-pages/readmes/apuntes.md', {})
-    .then(function(data) {$('#contenido > div > div').html('<h2>Licencia</h2><pre>' + data + '</pre>');})
+    .then(function(data) {$('#contenido > div > div').html(marked(data));})
     .fail(function() {$('#contenido > div > div').html(marked('## Error\nNo se pudo cargar contenido.'));})
   };
 
   function loadLicence() {
     $('#contenido > div > div').html('<p class="text-center"><i class="fa fa-spinner fa-spin"></i></span>');
     $.ajax('https://raw.githubusercontent.com/emilio93/Circuitos-Lineales-II/gh-pages/LICENSE', {})
-    .then(function(data) {$('#contenido > div > div').html('<h2>Licencia</h2><pre>' + data + '</pre>');})
+    .then(function(data) {$('#contenido > div > div').html(marked(data));})
     .fail(function() {$('#contenido > div > div').html(marked('## Error\nNo se pudo cargar contenido.'));})
   };
 
