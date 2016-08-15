3208cd917a0066c0304b38dd4ed757987231f642
Emilio Rojas <emilio93@gmail.com>
2016-08-14 21:07:01 -0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  loadInicio();
+  loadLicencia();
+  showPage();
+
+  $('nav>ul>a').click(function(){
+    setTimeout(function() {window.scrollTo(0, 0);}, 1);
+  });
+
+  $(window).on('hashchange',function(){
+    showPage(window.location.hash.substr(1));
+  });
+
+  function loadInicio() {
+    $('#readme').html('<p class="text-center"><i class="fa fa-spinner fa-spin"></i></span>');
+    $.ajax('https://raw.githubusercontent.com/emilio93/Circuitos-Lineales-II/master/README.md', {})
+    .then(function(data) {$('#readme').html(marked(data));})
+    .fail(function() {$('#readme').html(marked('## Inicio\nNo se pudo cargar contenido.'));})
+  }
+
+  function loadLicencia() {
+    $('#license').html('<p class="text-center"><i class="fa fa-spinner fa-spin"></i></span>');
+    $.ajax('https://raw.githubusercontent.com/emilio93/Circuitos-Lineales-II/master/LICENSE', {})
+    .then(function(data) {$('#license').html('<pre>' + data + '</pre>');})
+    .fail(function() {$('#license').html(marked('## Licencia\nNo se pudo cargar la licencia.'));})
+  }
+
+  function showPage(id) {
+    if (typeof id === 'undefined' || id.length <= 1) {
+      var hash = window.location.hash;
+      hash = hash.length>1? hash.substr(1): 'inicio';
+      return showPage(hash);
+    }
+    var pageSet = false;
+    $('section').each(function(index) {
+      if ($(this).attr('id') === id) {
+        $(this).show();
+        pageSet = true;
+      } else {
+        $(this).hide();
+      }
+    });
+    if (!pageSet) {
+      $('#errorPage').show();
+    }
+  }
+});
